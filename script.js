70a5d3681df3aa00cb97294c53133a2204cba82d
adriijan <adriana.domorakova@gnj.cz>
2024-02-08 19:12:01 +0100

    oprava

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 document.getElementById("submit").addEventListener("click", (event) => {
-    event.preventDefault(); 
-    
+    event.preventDefault();
+
     const emailInput = document.getElementById("email");
     const errorMessage = document.getElementById("error_message");
     const errorIcon = document.getElementById("error_icon");
     const emailBox = document.getElementById("email");
     const submitButton = document.getElementById("submit");
     const email = emailInput.value.trim();
-    
+
     if (email === "") {
         errorMessage.innerText = "Email Address is required."; // zobrazení chybové hlášky
         errorIcon.style.display = "inline-block"; // Zobrazí ikonu chyby
         emailBox.style.border = "2px solid hsl(0, 67%, 68%)"; // změna barvy rámečku
-        submitButton.style.top = "1.32em"; // posun pozice ikony
-    } 
-    
+        submitButton.style.top = "1.5px"; // posun pozice ikony
+    }
+
     else if (!isValidEmail(email)) {
         errorMessage.innerText = "Please provide a valid email.";
         errorIcon.style.display = "inline-block"; // 
         emailBox.style.border = "2px solid hsl(0, 67%, 68%)";
-        submitButton.style.top = "1.32em"; 
-    } 
+        submitButton.style.top = "1.5px";
+    }
 
     else {
         errorMessage.innerText = "";
-        errorIcon.style.display = "none"; 
-        emailBox.style.border = ""; 
+        errorIcon.style.display = "none";
+        emailBox.style.border = "";
         submitButton.style.top = "";
         emailBox.style.backgroundColor = "hsla(0, 36%, 70%, 0)";
     }
